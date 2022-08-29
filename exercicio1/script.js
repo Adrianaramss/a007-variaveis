5ca3a4f0ca1548af72f7ce9a3a5e70b1226aa474
Adriana <111310311+Adrianaramss@users.noreply.github.com>
2022-08-29 18:00:22 -0300

    Delete script.js

--- exercicio1/script.js
+++
@@ -1,7 +0,0 @@
-const Nome = prompt ("Qual seu nome?")
-let Idade = prompt ("Qual sua idade?")
-console.log (typeof Nome, typeof Idade)
-
-console.log("Olá", Nome, "você tem", Idade, "anos")
-////
-
